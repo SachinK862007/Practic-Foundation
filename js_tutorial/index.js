96fe3a941cd2438e6bb184082a3bef6349351cf0
Sachin Gowda K <sk911862007@gmail.com>
2026-09-03 16:00:46 +0530

    a step to learn js

---
+++ js_tutorial/index.js
@@ -0,0 +1,13 @@
+//console.log(`Hello World!`);  //output command
+//console.log(`I love pizza!`);
+//
+//window.alert('This is a Web');
+//window.alert('This is not a Web');
+
+//this is a comment for single line 
+/*
+this is a 
+multi line comment
+*/
+
+

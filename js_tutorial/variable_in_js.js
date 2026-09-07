956128afdf0e4b1d4357ec0a1764d15b592f5990
Sachin Gowda K <sk911862007@gmail.com>
2026-09-07 19:53:42 +0530

    done

--- js_tutorial/variable_in_js.js
+++ js_tutorial/variable_in_js.js
@@ -5,15 +5,16 @@ let z1, z2, z3, z4;
 y = 20;
 x = 100;
 
-console.log(x);
+console.log(`you are ${y} years old`);
 console.log(y);
-
-z1 = x + y;
-z2 = x - y;
-z3 = x * y;
-z4 = z / y;
-
-console.log(z1)
-console.log(z2)
-console.log(z3)
-console.log(z4)
+let z = 25
+//z1 = x + y;
+//z2 = x - y;
+//z3 = x * y;
+//z4 = z / y;
+//
+//console.log(z1);
+//console.log(z2);
+//console.log(z3);
+//console.log(z4);
+console.log(`you ${z}`)

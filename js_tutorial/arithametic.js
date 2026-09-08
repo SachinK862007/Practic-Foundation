f4268e0b2b8c685a3a68bfe08dcbaac1b0809740
Sachin Gowda K <sk911862007@gmail.com>
2026-09-08 18:02:31 +0530

    ongoing with inputs from the user in js

---
+++ js_tutorial/arithametic.js
@@ -0,0 +1,20 @@
+//let students = 30;
+//
+//students = students + 7;
+//students = students - 7;
+//students = students * 2;
+//students = students / 2;
+//students = students ** 2;
+//let expstudents = students % 2; //most recomended to use a seprate variable for the modules operator
+
+//let result = 1 + 5 * 3 + 4 ** 2;
+//console.log(result)
+//
+//console.log(students)
+
+
+// this is a user input practice
+
+let username;
+
+username = window.prompt(`What is your name? `);
